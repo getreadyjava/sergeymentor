4cbdea5c473501b7b46f8629f6874a426e7aa175
Anton Kuzmin <jirafigma@gmail.com>
2025-12-04 23:05:02 +0300

    Изменил javadoc для контроллера; добавил lombok.NonNull; избавился от проверок в методах на null; избавился от блоков проверки на исключения try, catch

--- src/main/java/ru/preworking/drill/controller/EmployeeController.java
+++ src/main/java/ru/preworking/drill/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import lombok.extern.slf4j.Slf4j;
+import lombok.NonNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +14,7 @@ import ru.preworking.drill.domain.Employee;
 import ru.preworking.drill.service.EmployeeService;
 
 /**
- * CRUD на сотрудников
- * POST, GET, PUT, DELETE
+ * Контроллер, обрабатывающий запросы к ресурсу и вызывающий сервис
  */
 @Slf4j
 @RestController
@@ -28,84 +28,43 @@ public class EmployeeController {
 
     @GetMapping
     public ResponseEntity<List<Employee>> getAllEmployees(){
-        try {
-            List<Employee> employees = service.getAll();
-
-            return new ResponseEntity<>(employees, HttpStatus.OK);
-        } catch (Exception e /*Будет Custom Exception*/) {
-
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        List<Employee> employees = service.getAll();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
     @GetMapping("/{employeeId}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable("employeeId") String id){
-        try {
-            if (id == null) {
-
-                return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-            }
-
-            Employee employee = (Employee) service.getById(id);
-System.out.println("employee: " + employee);
-            return new ResponseEntity<>(employee, HttpStatus.OK);
-        } catch (Exception e /*Будет Custom Exception*/) {
-            System.out.println("catched exception: " + e.getMessage());
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<Employee> getEmployeeById(@NonNull @PathVariable("employeeId") String id){
+        Optional<Employee> employee = service.getById(id);
+        return new ResponseEntity<>(employee.get(), HttpStatus.OK);
     }
 
     // @todo При создании репозитория, получение id со стороны клиента убрать
     @PostMapping
     public ResponseEntity<Map<String, Boolean>> createEmployee(@RequestBody Employee employee){
-         try {
-
-            service.add(employee);
-
-            return new ResponseEntity<>(Map.of("success", true), HttpStatus.OK);
-        } catch (Exception e /*Будет Custom Exception*/) {
-
-            return new ResponseEntity<>(Map.of("success", false), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        service.add(employee);
+        return new ResponseEntity<>(Map.of("success", true), HttpStatus.OK);
     }
 
     @PutMapping("/{employeeId}")
-    public ResponseEntity<Map<String, Boolean>> updateEmployee(@PathVariable("employeeId") String id, @RequestBody Employee employee){
-        if (id == null) {
-
-            return new ResponseEntity<>(Map.of("success", false), HttpStatus.BAD_REQUEST);
-        } else {
-
-            Employee employeeOptional = (Employee) service.getById(id);
-
-            if (employeeOptional != null) {
-                service.update(employee);
-                return new ResponseEntity<>(Map.of("success", true), HttpStatus.OK);
-            }
-
-            return new ResponseEntity<>(Map.of("success", false), HttpStatus.NOT_FOUND);
+    public ResponseEntity<Map<String, Boolean>> updateEmployee(@NonNull @PathVariable("employeeId") String id, @RequestBody Employee employee){
+        Optional<Employee> updatedEmployee = service.getById(id);
+        if (updatedEmployee.isPresent()) {
+            service.update(employee);
+            return new ResponseEntity<>(Map.of("success", true), HttpStatus.OK);
         }
+
+        return new ResponseEntity<>(Map.of("success", false), HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/{employeeId}")
-    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable("employeeId") String id){
-        if (id == null) {
-
-            return new ResponseEntity<>(Map.of("success", false), HttpStatus.BAD_REQUEST);
-        } else {
-            service.delete(id);
-            return new ResponseEntity<>(Map.of("success", true), HttpStatus.OK);
-        }
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@NonNull @PathVariable("employeeId") String id){
+        service.delete(id);
+        return new ResponseEntity<>(Map.of("success", true), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete-list")
-    public ResponseEntity<Map<String, Boolean>> deleteManyEmployees(@RequestBody List<String> ids){
-        if (ids == null) {
-
-            return new ResponseEntity<>(Map.of("success", false), HttpStatus.BAD_REQUEST);
-        } else {
-            service.delete(ids);
-            return new ResponseEntity<>(Map.of("success", true), HttpStatus.OK);
-        }
+    public ResponseEntity<Map<String, Boolean>> deleteManyEmployees(@NonNull @RequestBody List<String> ids){
+        service.delete(ids);
+        return new ResponseEntity<>(Map.of("success", true), HttpStatus.OK);
     }
 }
